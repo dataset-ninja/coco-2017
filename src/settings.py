89d218e58a4f32d27f049584c74a3711b7cabf7d
Ars Vladimirov <grokhael@gmail.com>
2023-09-25 14:53:48 +0600

    preview upd

--- src/settings.py
+++ src/settings.py
@@ -41,7 +41,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://cocodataset.org/#home"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 1183861
+PREVIEW_IMAGE_ID: int = 4036507
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/coco-2017"
